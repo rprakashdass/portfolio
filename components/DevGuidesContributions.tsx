fc308afe8df2ac4b7a16d884f7735dd388edd040
rprakashdass <rprakashdass@gmail.com>
2025-10-20 23:16:58 +0530

    feat: Refactor DevGuidesContributions component to enhance tutorial display and add show more/less functionality

--- components/DevGuidesContributions.tsx
+++ components/DevGuidesContributions.tsx
@@ -1,53 +1,72 @@
+"use client";
+
 import { useState, useRef } from 'react'
 import { tutorials, type Tutorial } from '@/data'
 import ContributionsCardGenerator from './ContributionsCardGenerator'
 import React from 'react'
-import SeeMore from './utility/SeeMore'
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 
 export const DevGuidesContributions = () => {
+    const showLessRef = useRef<HTMLParagraphElement | null>(null);
+    const [showAll, setShowAll] = useState<boolean>(false);
+
+    const handleShowMore = () => {
+        setShowAll(true);
+    }
+
+    const handleShowLess = () => {
+        showLessRef.current?.scrollIntoView({ behavior: 'smooth' })
+        setShowAll(false);
+    }
+
+    const itemsData = showAll ? tutorials : tutorials.slice(0, 2);
 
     return (
         <>
-            <SeeMore data={tutorials}>
+            <div>
+                <div className="py-10 text-center text-white">
+                    <h1 className="mb-8 font-extrabold text-5xl">
+                        <span className="bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 text-transparent">
+                            Tutorials & Resources
+                        </span>
+                    </h1>
+                    <p
+                        ref={showLessRef}
+                        className="mx-auto mb-6 max-w-3xl text-gray-400 text-lg leading-relaxed">
+                        Explore tutorials on programming, algorithms, and NLP with practical insights. Learn or contribute today!
+                    </p>
+                </div>
 
-                {(
-                    ({ itemsData, showLessRef }: { itemsData: Tutorial[], showLessRef: React.RefObject<HTMLParagraphElement> }) => {
-                        return (
-                            <div>
+                <div
+                    className={`flex flex-col md:flex-row gap-10 justify-center transition-opacity duration-500'}`}
+                >
+                    {itemsData.map((item: Tutorial) => (
+                        <ContributionsCardGenerator
+                            key={item.id}
+                            title={item.title}
+                            description={item.description}
+                            link={item.link}
+                        />
+                    ))}
+                </div>
+            </div>
 
-                                <div className="py-10 text-center text-white">
-                                    <h1 className="mb-8 font-extrabold text-5xl">
-                                        <span className="bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 text-transparent">
-                                            Tutorials & Resources
-                                        </span>
-                                    </h1>
-                                    <p
-                                        ref={showLessRef}
-                                        className="mx-auto mb-6 max-w-3xl text-gray-400 text-lg leading-relaxed">
-                                        Explore tutorials on programming, algorithms, and NLP with practical insights. Learn or contribute today!
-                                    </p>
-                                </div>
-
-                                <div
-                                    className={`flex flex-col md:flex-row gap-10 justify-center transition-opacity duration-500'}`}
-                                >
-                                    {itemsData.map((item: Tutorial) => (
-                                        <ContributionsCardGenerator
-                                            key={item.id}
-                                            title={item.title}
-                                            description={item.description}
-                                            link={item.link}
-                                        />
-                                    ))}
-                                </div>
-
-                            </div>
-                        )
+            {/* Show More/Less Button */}
+            <div className="flex justify-center opacity-45 py-20">
+                <button onClick={showAll ? handleShowLess : handleShowMore} className="inline-flex gap-4 font-bold text-2xl">
+                    {showAll ?
+                        <span className="inline-flex">
+                            Show Less
+                            <AiFillCaretUp size={32} />
+                        </span>
+                        :
+                        <span className="inline-flex">
+                            Show More
+                            <AiFillCaretDown size={32} />
+                        </span>
                     }
-                )}
-
-            </SeeMore>
-
+                </button>
+            </div>
         </>
     );
 }
